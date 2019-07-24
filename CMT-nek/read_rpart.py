735b16a6536b692627b903788f31e6a0ee2ffac1
Rahul Koneru <rahul9babu@gmail.com>
2019-07-24 19:46:22 -0400

    Added option to read on a per particle basis

--- CMT-nek/read_rpart.py
+++ CMT-nek/read_rpart.py
@@ -3,15 +3,16 @@
 # In the rpart* files, each particle has 42 entries each of size 'prec'
 # with the very first entry being the total no.of particles.
 # DATA LAYOUT
-# Particle Position [ x  y  z] = rows 2 3 4 , 5-13 history values
-# Particle Velocity [vx vy vz] = rows 14 15 16, 17-25 history values
-# Fluid Velocity    [ux uy uz] = rows 26 27 28, 29-37 history values
-# Diameter = row 38
-# Super-particle loading = row 39
-# Temperature = row 40
-# Particle id = rows 41 42 43
+# Number of particles = 0th entry
+# Particle Position [ x  y  z] = rows 1 2 3 , 4-12 history values
+# Particle Velocity [vx vy vz] = rows 13 14 15, 16-24 history values
+# Fluid Velocity    [ux uy uz] = rows 25 26 27, 28-36 history values
+# Diameter = row 37
+# Super-particle loading = row 38
+# Temperature = row 39
+# Particle id = rows 40 41 42
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import struct
 
@@ -22,11 +23,14 @@ fid = open('rpart00001_old', 'rb')
 f = fid.read()  # Binary read
 prec = 'f' # Prescision
 size = struct.calcsize(prec)
-#npcls = struct.unpack(prec, f[0:size])          # No.of pcls
+npcls = struct.unpack(prec, f[0:size])          # No.of pcls
 #x = struct.unpack(prec, f[4:8]) # x-coords
 #y = struct.unpack(prec, f[8:12]) # y-coords
 #z = struct.unpack(prec, f[12:16]) # z-coords
 
+for i in range(2):
+    dd = list(struct.unpack(42*prec,f[i*42*size+size:(i+1)*42*size+size]))
+    print(dd)
 d = list(struct.iter_unpack(prec, f))
 npcls = d[0]          # No.of pcls
 x = d[1:len(d):42] # x-coords
